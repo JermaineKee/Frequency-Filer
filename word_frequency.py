02a30fa54d0da5bada45329c227b0670a0d03d8d
Jermaine Kee <godson919@icloud.com>
2015-10-02 11:59:29 -0400

    revised

--- word_frequency.py
+++ word_frequency.py
@@ -1,26 +1,24 @@
 import re
 
+
 def word_frequency(text):
-    count = re.sub(r'[A-Za-z\s]', "", text).lower().split()
+    word = re.sub(r'[,.!-0-9]', "", text).lower().split()
 
+    count = {}
 
-    word_count = {}
-
-
-
-    for x in words:
-        if x in word_count:
-            word_count[x] += 1
+    for x in word:
+        if x in count:
+            count[x] += 1
         else:
-            word_count[x] = 1
-    return word_count
+            count[x] = 1
+    return count
 
 
 def main():
-    text = open('sample.txt')
-    text.read()
-    for final_count in sorted(word_frequency(text).items(), key=lambda c: c[1], reverse=True)[:20]:
-        print(final_count)
+    with open('sample.txt') as s:
+        text = s.read()
+        for final_count in sorted(word_frequency(text).items(), key=lambda c: c[1], reverse=True)[:20]:
+            print(final_count)
 
 if __name__ == '__main__':
     main()
